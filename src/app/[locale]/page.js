27feeb39da538c474531792e3c4ca3136018f962
Sol <sol6801@gmail.com>
2025-01-21 18:16:01 +0100

    remove number from messages

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -61,17 +61,17 @@ const WaveSVG = () => (
 
 export default function Home({ params: paramsPromise }) {
   const t = useTranslations("Home");
-const WhatsAppButton = () => (
-  <a
+  const WhatsAppButton = () => (
+    <a
       href={t('whatsapp')}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all duration-300 ease-in-out z-50 flex items-center justify-center"
-    aria-label="Contactar por WhatsApp"
-  >
-    <WhatsAppIcon className="h-8 w-8" />
-  </a>
-);
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all duration-300 ease-in-out z-50 flex items-center justify-center"
+      aria-label="Contactar por WhatsApp"
+    >
+      <WhatsAppIcon className="h-8 w-8" />
+    </a>
+  );
   const params = use(paramsPromise); 
   const locale = params?.locale;
 
